2dc0dfd4dd9b1c02fd54c7a4bb1510d42a470b70
ASahak <arthur.sahakyan96@gmail.com>
2025-10-21 14:52:32 +0400

    chore(setup): finished setup of monorepo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', './client/.next'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
