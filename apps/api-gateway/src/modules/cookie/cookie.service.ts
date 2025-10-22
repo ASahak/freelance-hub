ce89e4dbc42ef88f3bdcb2ebce342a763c5cec40
ASahak <arthur.sahakyan96@gmail.com>
2025-10-22 12:43:43 +0400

    fix(cookie): fixed cookie same site issue

--- apps/api-gateway/src/modules/cookie/cookie.service.ts
+++ apps/api-gateway/src/modules/cookie/cookie.service.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
 
 @Injectable()
 export class CookieService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor() {}
 
   setTokenCookie(res: Response, token: string): void {
-    const isProduction = this.configService.get('NODE_ENV') === 'production';
     res.cookie('access_token', token, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? 'none' : 'lax',
+      secure: true,
+      sameSite: 'none',
       path: '/',
     });
   }
 
   clearTokenCookie(res: Response): void {
-    const isProduction = this.configService.get('NODE_ENV') === 'production';
     res.clearCookie('access_token', {
       httpOnly: true,
       path: '/',
-      sameSite: isProduction ? 'none' : 'lax',
-      secure: this.configService.get('NODE_ENV') === 'production',
+      sameSite: 'none',
+      secure: true,
     });
   }
 }
